094bd23281fd69513f320a3650a75cf948c267d9
Klaster28 <89096914521@mail.ru>
2022-11-23 15:10:40 +0300

    Коммит по первой части

--- src/Flower.java
+++ src/Flower.java
@@ -3,60 +3,66 @@ public class Flower {
     private String flowerColor;
     private String country;
     private double cost;
-     int lifeSpan;
+    private int lifeSpan;
 
 
 
-    Flower(String name, String flowerColor, String country, double cost, int lifeSpan){
+    Flower(String name, String flowerColor, String country, double cost, int lifeSpan) {
 
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        if (name == null || name.isEmpty() || name.isBlank())
-            this.name = "Нет данных";
-        }else {
-        this.name = name;
     }
 
-
-    public String getFlowerColor() {
-        return flowerColor;
-    }
-
-    public void setFlowerColor(String flowerColor) {
-        if (flowerColor == null || flowerColor.isEmpty() || flowerColor.isBlank()) {
-            this.flowerColor = "белый";
-        } else {
-            this.flowerColor = flowerColor;
+        public String getName () {
+            return name;
         }
-
+        public String getFlowerColor () {
+            return flowerColor;
+        }
         public String getCountry () {
             return country;
         }
+        public double getCost () {
+            return cost;
+        }
+        public int getLifeSpan () {
+            return lifeSpan;
+        }
 
-        public void setCountry (String country){
+        public void setName (String name){
+            if (name == null || name.isEmpty() || name.isBlank()){
+                this.name = "Нет данных";
+            } else {
+                this.name = name;
+            }
+        }
+
+
+        public void setFlowerColor (String flowerColor) {
+            if (flowerColor == null || flowerColor.isEmpty() || flowerColor.isBlank()) {
+                this.flowerColor = "белый";
+            } else {
+                this.flowerColor = flowerColor;
+            }
+        }
+
+        public void setCountry (String country) {
             if (country == null || country.isEmpty() || country.isBlank()) {
                 this.country = "Россия";
             } else {
                 this.country = country;
             }
-
         }
 
-        public double getCost () {
-            return cost;
-        }
-
-        public void setCost ( double cost){
+        public void setCost ( double cost) {
             if (cost < 0) {
                 this.cost = 0;
             } else {
                 this.cost = cost;
             }
+        }
 
-
+        public void setLifeSpan(int lifeSpan) {
+            this.lifeSpan = lifeSpan;
         }
     }
-}
+
 
--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,19 @@
 public class Main {
-    private static void printInfo(Flower flower) {
-        System.out.println("Название цветка - " + flower.getName() + ". Цвет бутона - " + flower.getFlowerColor() + ". Страна происхожения - " + flower.getCountry() + ". Стоимость за штуку - " + flower.getCost() + ". Количество дней стояния - " + flower.getLifeSpan() + ". ");
-    }
-    public Main() {
-    }
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
-    Flower roza = new Flower("Роза обыкновенная", " ", "Голландия", 55.59, -1);
-    Flower hrizantema = new Flower("Хризантема", " ", " ", 15, 5);
-    Flower pion = new Flower("Пион", " ", "Англия", 69.9, 1);
-    Flower gipsofila = new Flower("Гипсофила", " ", "Турция", 19.5, 10);
+        Flower roza = new Flower("Роза обыкновенная", " ", "Голландия", 55.59, -1);
+        Flower hrizantema = new Flower("Хризантема", " ", " ", 15, 5);
+        Flower pion = new Flower("Пион", " ", "Англия", 69.9, 1);
+        Flower gipsofila = new Flower("Гипсофила", " ", "Турция", 19.5, 10);
 
-    printInfo(roza);
-    printInfo(hrizantema);
-    printInfo(pion);
-    printInfo(gipsofila);
+        printInfo(roza);
+        printInfo(hrizantema);
+        printInfo(pion);
+        printInfo(gipsofila);
     }
 
-   // private static void printInfo(Flower flower) {
-    //    System.out.println("Название цветка - " + flower.getName() + ". Цвет бутона - " + flower.getFlowerColor() + ". Страна происхожения - " + flower.getCountry() + ". Стоимость за штуку - " + flower.getCost() + ". Количество дней стояния - " + flower.getLifeSpan() + ". ");
-    //}
+   private static void printInfo(Flower flower) {
+      System.out.println("Название цветка - " + flower.getName() + ". Цвет бутона - " + flower.getFlowerColor() + ". Страна происхожения - " + flower.getCountry() + ". Стоимость за штуку - " + flower.getCost() + ". Количество дней стояния - " + flower.getLifeSpan() + ". ");
+    }
 }
