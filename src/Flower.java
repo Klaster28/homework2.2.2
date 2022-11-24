18015bae759b81c3b16a9ecf79c9e7a462ab0d0a
Klaster28 <89096914521@mail.ru>
2022-11-24 15:05:20 +0300

    Коммит после ДЗ1 2.2

--- src/Flower.java
+++ src/Flower.java
@@ -8,68 +8,78 @@ public class Flower {
     private int lifeSpan;
 
 
-
     Flower(String name, String flowerColor, String country, double cost, int lifeSpan) {
-        this.name = name;
-        this.flowerColor = flowerColor;
-        this.country = country;
-        this.cost = cost;
-        this.lifeSpan = lifeSpan;
 
-    }
-
-        public String getName () {
-            return name;
+        if (name == null || name.isEmpty() || name.isBlank()) {
+            this.name = "Нет данных";
+        } else {
+            this.name = name;
         }
-        public String getFlowerColor () {
-            return flowerColor;
+        if (flowerColor == null || flowerColor.isEmpty() || flowerColor.isBlank()) {
+            this.flowerColor = "белый";
+        } else {
+            this.flowerColor = flowerColor;
         }
-        public String getCountry () {
-            return country;
+        if (country == null || country.isEmpty() || country.isBlank()) {
+            this.country = "Россия";
+        } else {
+            this.country = country;
         }
-        public double getCost () {
-            return cost;
+        if (cost < 0) {
+            this.cost = 0;
+        } else {
+            this.cost = cost;
         }
-        public int getLifeSpan () {
-            return lifeSpan;
-        }
-
-        public void setName (String name){
-            if (name == null || name.isEmpty() || name.isBlank()){
-                this.name = "Нет данных";
-            } else {
-                this.name = name;
-            }
-        }
-
-
-        public void setFlowerColor (String flowerColor) {
-            if (flowerColor == null || flowerColor.isEmpty() || flowerColor.isBlank()) {
-                this.flowerColor = "белый";
-            } else {
-                this.flowerColor = flowerColor;
-            }
-        }
-
-        public void setCountry (String country) {
-            if (country == null || country.isEmpty() || country.isBlank()) {
-                this.country = "Россия";
-            } else {
-                this.country = country;
-            }
-        }
-
-        public void setCost ( double cost) {
-            if (cost < 0) {
-                this.cost = 0;
-            } else {
-                this.cost = cost;
-            }
-        }
-
-        public void setLifeSpan(int lifeSpan) {
+        if (lifeSpan < 0) {
+            this.lifeSpan = 0;
+        } else {
             this.lifeSpan = lifeSpan;
         }
+
+
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFlowerColor() {
+        return flowerColor;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public int getLifeSpan() {
+        return lifeSpan;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setFlowerColor(String flowerColor) {
+        this.flowerColor = flowerColor;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public void setLifeSpan(int lifeSpan) {
+        this.lifeSpan = lifeSpan;
+    }
+}
+
+
 
 
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
         public static void main(String[] args) {
 
-        Flower roza = new Flower("Роза обыкновенная", " ", "Голландия", 55.59, -1);
+        Flower roza = new Flower("Роза обыкновенная", " ", "Голландия", 55.59, 1);
         Flower hrizantema = new Flower("Хризантема", " ", " ", 15, 5);
         Flower pion = new Flower("Пион", " ", "Англия", 69.9, 1);
         Flower gipsofila = new Flower("Гипсофила", " ", "Турция", 19.5, 10);
@@ -11,9 +11,31 @@ public class Main {
         printInfo(hrizantema);
         printInfo(pion);
         printInfo(gipsofila);
+            System.out.println();
+        printCostBouquet(roza,hrizantema,pion,gipsofila,roza,pion);
     }
 
    private static void printInfo(Flower flower) {
       System.out.println("Название цветка - " + flower.getName() + ". Цвет бутона - " + flower.getFlowerColor() + ". Страна происхожения - " + flower.getCountry() + ". Стоимость за штуку - " + flower.getCost() + ". Количество дней стояния - " + flower.getLifeSpan() + ". ");
     }
+
+    private static void printCostBouquet(Flower... flowers) {
+            double totalCost = 0;
+            int minimumLifeSpan = Integer.MAX_VALUE;
+        for (Flower flower : flowers) {
+            if (flower.getLifeSpan() < minimumLifeSpan) {
+                minimumLifeSpan = flower.getLifeSpan();
+            }
+
+            totalCost += flower.getCost();
+            printInfo(flower);
+            System.out.println();
+        }
+        totalCost = totalCost * 1.1;
+        System.out.println("Букет стоит - " + totalCost + " рублей.");
+        System.out.println("Букет будет стоять - " + minimumLifeSpan + " дн.");
+
+    }
+
+
 }
